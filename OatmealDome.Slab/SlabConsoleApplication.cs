971a337bf0e875ebd0580fff6e169d664553d53d
OatmealDome <julian@oatmealdome.me>
2025-03-07 14:59:51 -0500

    SlabConsoleApplication: Simplify CreateBuilder and CreateHost

--- OatmealDome.Slab/SlabConsoleApplication.cs
+++ OatmealDome.Slab/SlabConsoleApplication.cs
@@ -4,15 +4,9 @@ namespace OatmealDome.Slab;
 
 public abstract class SlabConsoleApplication : SlabApplication<HostApplicationBuilder, IHost>
 {
-    protected override HostApplicationBuilder CreateBuilder(string[]? args)
-    {
-        return Host.CreateApplicationBuilder(args);
-    }
-
-    protected override IHost CreateHost(HostApplicationBuilder builder)
-    {
-        return builder.Build();
-    }
+    protected override HostApplicationBuilder CreateBuilder(string[]? args) => Host.CreateApplicationBuilder(args);
+    
+    protected override IHost CreateHost(HostApplicationBuilder builder) => builder.Build();
 
     protected override void SetupApplication(IHost host)
     {
