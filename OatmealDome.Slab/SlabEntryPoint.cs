4a9f3802e07abaac9375180da92ef6615c8caa53
OatmealDome <julian@oatmealdome.me>
2025-03-07 19:05:13 -0500

    SlabEntryPoint: Wait for any attached debugger to intiailize before continuing If an exception occurs during early start up, the debugger might not be fully ready, and will freeze when trying to retrieve exception information.

--- OatmealDome.Slab/SlabEntryPoint.cs
+++ OatmealDome.Slab/SlabEntryPoint.cs
@@ -1,9 +1,34 @@
+using System.Diagnostics;
+
 namespace OatmealDome.Slab;
 
 public static class SlabEntryPoint
 {
     public static void RunApplication<T>(string[]? args) where T : SlabApplicationBase, new()
     {
+#if DEBUG
+        DateTime startTime = DateTime.UtcNow;
+
+        while (!Debugger.IsAttached)
+        {
+            Thread.Sleep(100);
+            
+            TimeSpan difference = DateTime.UtcNow.Subtract(startTime);
+
+            // If the debugger is taking too long to attach, just give up and proceed to start the application.
+            if (difference.TotalSeconds > 5)
+            {
+                break;
+            }
+        }
+
+        // Enforce an additional sleep to ensure that the debugger is fully initialized.
+        if (Debugger.IsAttached)
+        {
+            Thread.Sleep(1000);
+        }
+#endif
+        
         Type appType = typeof(T);
         T app = (T)Activator.CreateInstance(appType)!;
         
